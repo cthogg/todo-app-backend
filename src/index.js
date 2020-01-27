bc3fbabaa8ae40b196a56991dc03958a863800d4
cthogg <cthogg@users.noreply.github.com>
2020-01-27 07:19:29 +0100

    feat: add back cors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 import cors from 'cors';
 import 'dotenv/config';
 import request from 'supertest'
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //TODO: add title as well
