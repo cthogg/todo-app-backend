056aa4628671c187890b28b19ffd49a1a905b036
cthogg <cthogg@users.noreply.github.com>
2020-01-27 07:09:12 +0100

    feat: remove cors from backend

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import express from 'express';
 const app = express();
 import cors from 'cors';
 import 'dotenv/config';
-import uuidv4 from 'uuid/v4';
 import request from 'supertest'
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //TODO: add title as well
